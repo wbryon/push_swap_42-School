0e72c951f2c09d09116454b2354888781f734fe5
Willodean Bryon <wbryon@si-d5.kzn.21-school.ru>
2021-04-20 20:15:40 +0300

    change name to sort.c

---
+++ sort.c
@@ -0,0 +1,74 @@
+#include "push_swap.h"
+
+int	sort_three(t_stack *stack)
+{
+	if (stack->a[0] > stack->a[1] && stack->a[1] > stack->a[2] && stack->a[2] < stack->a[0])
+	{
+		sa(stack);
+		rra(stack);
+	}
+	if (stack->a[0] > stack->a[1] && stack->a[1] < stack->a[2] && stack->a[2] < stack->a[0])
+		ra(stack);
+	if (stack->a[0] < stack->a[1] && stack->a[1] > stack->a[2] && stack->a[2] < stack->a[0])
+		rra(stack);
+	if (stack->a[0] > stack->a[1] && stack->a[1] < stack->a[2] && stack->a[2] > stack->a[0])
+		sa(stack);
+	if (stack->a[0] < stack->a[1] && stack->a[1] > stack->a[2] && stack->a[2] > stack->a[0])
+	{
+		rra(stack);
+		sa(stack);
+	}
+	return (0);
+}
+
+int	sort_five(t_stack *stack)
+{
+	int	i;
+
+	i = -1;
+	pb(stack);
+	pb(stack);
+	sort_three(stack);
+	if (stack->a[0] < stack->b[0])
+	{
+		if (stack->b[0] < stack->a[2])
+		{
+			rra(stack);
+			pa(stack);
+			ra(stack);
+			ra(stack);
+		}
+		else
+		{
+			pa(stack);
+			ra(stack);
+		}
+	}
+	else
+		pa(stack);
+	if (stack->a[0] < stack->b[0])
+	{
+		if (stack->b[0] < stack->a[1])
+		{
+			pa(stack);
+			sa(stack);
+		}
+		else if (stack->b[0] < stack->a[2])
+		{
+			ra(stack);
+			pa(stack);
+			sa(stack);
+			rra(stack);
+		}
+		else if (stack->b[0] < stack->a[3])
+		{
+			rra(stack);
+			pa(stack);
+			ra(stack);
+			ra(stack);
+		}
+	}
+	else
+		pa(stack);
+	return (0);
+}
